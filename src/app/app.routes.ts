a18b6e6fd960e782ea5abb81edd7ee17acae8f73
antonBlagodarnyy <antonbgi98@gmail.com>
2025-12-18 09:38:55 +0100

    added redirect from base path

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { LocationsComponent } from './sections/locations/locations.component';
 import { EpisodesComponent } from './sections/episodes/episodes.component';
 
 export const routes: Routes = [
+  {path: '', redirectTo: '/characters', pathMatch: 'full'},
   { path: 'characters', component: CharactersComponent },
   { path: 'locations', component: LocationsComponent },
   { path: 'episodes', component: EpisodesComponent },
